e4303042bba93f58e366b574e5fdc08d64620ad4
nethcor <nethcor9@gmail.com>
2024-10-24 15:22:50 -0400

    FASE GREEN. codigo minimo para que pase la prueba substract

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public int add(int arg1, int arg2) {
         return arg1 + arg2;
     }
+    
+    public int subtract(int arg1, int arg2) {
+        return 2;
+    }
 }
